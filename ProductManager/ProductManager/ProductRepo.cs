e98096e1f8414f5ed8be491e206eddd2355a5eda
anaghjois <76957947+anaghjois@users.noreply.github.com>
2023-01-25 10:02:08 +0530

    Add files via upload

---
+++ ProductManager/ProductManager/ProductRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProductManager
+{
+   public interface IProduct
+    {
+        void AddProducts(Product pro);
+        void DeleteProduct(int id);
+        void UpdateProduct(Product pro);
+        List<Product> GetProducts();
+    }
+    public class ProductRepo : IProduct
+    {
+        public void AddProducts(Product pro)
+        {
+            var context = new ProductEntities();
+            context.Products.Add(pro);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var context = new ProductEntities();
+            var found = context.Products.FirstOrDefault(d => d.ProductId == id);
+            if (found == null) throw new Exception("Product Not found");
+            context.Products.Remove(found);
+            context.SaveChanges();
+        }
+
+        public List<Product> GetProducts()
+        {
+            var context = new ProductEntities();
+            var data = context.Products.ToList();
+            return data;
+        }
+
+        public void UpdateProduct(Product pro)
+        {
+            var context = new ProductEntities();
+            var found = context.Products.FirstOrDefault(d => d.ProductId == pro.ProductId);
+            if (found == null) throw new Exception("Product Not found");
+            found.ProductPrice = pro.ProductPrice;
+            found.Quantity = pro.Quantity;
+            context.SaveChanges();
+        }
+    }
+}
